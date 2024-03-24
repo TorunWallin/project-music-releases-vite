66fd489c5fb4d0c8b352f90a0513b342ace3ef67
Torunwallin <itorun@me.com>
2024-03-24 16:34:12 +0100

    Update index.html, App.jsx, and styles.css

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// This component displays a static title for the application.
+function Header() {
+  return (
+    <header className="app-header">
+      <h1>Music Releases</h1>  {/* Displaying the static title using a h1 tag. */}
+    </header>
+  );
+}
+
+export default Header;  // Exporting the Header component to be used in other files.
